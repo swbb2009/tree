9649da30d4c7e87655934b35e1c51dffffa29635
limingda <limingda@aspirecn.com>
2021-09-30 15:14:05 +0800

    编外提交1

--- src/main/java/com/example/tree/JavaTreeApplication.java
+++ src/main/java/com/example/tree/JavaTreeApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JavaTreeApplication {
+	
+	private String name;
 
     public static void main(String[] args) {
         SpringApplication.run(JavaTreeApplication.class, args);
